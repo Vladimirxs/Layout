1592679241f5c1bcfc65def941d57db4ecedaa97
Vladimirxs <vova.dobrynin.04@bk.ru>
2026-06-22 13:10:19 +0300

    layout

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Shop from './assets/ShopGuide/Shop';
+import './App.css';
+
+function App() {
+  return (
+    <Shop/>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+export default App
---
+++ src/assets/ShopGuide/Shop.jsx
@@ -0,0 +1,51 @@
+import './Shop.scss';
+
+
+function Shop() {
+    return (
+        <div className="img-shop">
+            <img src="block 1/Layer 1 copy 1.png" alt="" />
+            <div className='top-panel'>
+            <div className="icon">
+                <img src="block 1/Property 1=VK.png" alt="D" />
+                <img src="block 1/Property 1=Twitter.png" alt="" />
+                <img src="block 1/Property 1=Instagram.png" alt="" />
+                <img src="block 1/Property 1=Faebook.png" alt="" />
+                <img src="block 1/Property 1=OK.png" alt="" />
+            </div>
+            <button className='btn-shop'>СТАТЬ ПАРТНЕРОМ</button>
+            </div>
+
+            <div className='phone'>
+            <img src="block 1/Phone.png" alt="" />
+            </div>
+
+            <div className='menu-shop'>
+                <img src="block 1/Frame 3.png" alt="" />
+            </div>
+
+            <div className='menu-entry'>
+                <img src="block 1/Frame 2.png" alt="" />
+            </div>
+
+            <button className='btn-app'>
+                <img  src="block 1/Vector.png" alt="" />
+                <div className='btn-text'>
+                    <span className="btn-subtitle">Download on the</span>
+                    <span className="btn-title">App Store</span>
+                </div>
+            </button>
+
+
+        </div>
+
+
+    )
+}
+
+
+
+
+
+
+export default Shop;
